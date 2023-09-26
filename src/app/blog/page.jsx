7f451d951362c677feac573e9f95634d0a4fc41a
ar.sam.cossa@gmail.com <ar.sam.cossa@gmail.com>
2023-09-26 21:47:35 +0200

    all bugs fixed from feching server side data fecthing

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
     cache: "no-store",
   });
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    return res.json();
   }
 
   return res.json();
