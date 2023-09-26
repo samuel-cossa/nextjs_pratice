073617b4869ee32fd94de940e49311fce7534cc6
ar.sam.cossa@gmail.com <ar.sam.cossa@gmail.com>
2023-09-26 22:15:20 +0200

    Ajustando os links do footer e eliminando o bug do client side data fetching

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -7,17 +7,18 @@ import styles from './page.module.css'
 const Dashboard = () => {
   
   const [data,setData] = useState([])
+  const [err, setErr] = useState(false);
   const [isLoading, setIsLoading] = useState(false)
   
   useEffect(()=>{
     const getData = async () =>{
       setIsLoading(true)
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
         cache: 'no-store'
       });
 
       if (!res.ok) {
-        return notFound(true)
+        setErr(true)
       }
       const data = await res.json()
       setData(data)
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "./footer.module.css";
 import Image from "next/image";
+import Link from 'next/link';
 
 
 
@@ -9,18 +10,18 @@ const Footer = () => {
     <div className={styles.container}>
       <div>©2023 Usolutions. All rights reserved.</div>
       <div className={styles.social}>
-        <a href="https://facebook.com">
+        <Link href="https://facebook.com">
           <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Usolutio Dev Facebook Account" />
-        </a>
-        <a href="https://google.com">
+        </Link>
+        <Link href="https://google.com">
           <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Usolutio Dev Instagram Account" />
-        </a>
-        <a href="https://google.com">
+        </Link>
+        <Link href="https://google.com">
           <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Usolutio Dev Twitter Account" />
-        </a>
-        <a href="https://youtube.com">
+        </Link>
+        <Link href="https://youtube.com">
           <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Usolutio Dev Youtube Channel" />
-        </a>
+        </Link>
       </div>
     </div>
   )
