b080e7b92d39920726fb76f0c9596b92c1c11cfa
ar.sam.cossa@gmail.com <ar.sam.cossa@gmail.com>
2023-09-26 04:09:01 +0200

    Server side data fetching

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,32 +1,47 @@
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: 'no-store'
+  });
+
+  if (!res.ok) {
+    return notFound()
+  }
+
+  return res.json();
+}
 
 
-const BlogPost = () => {
-  // const data = await getData(params.id);
+const BlogPost = async ({params}) => {
+  
+  const data = await getData(params.id);
+  
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>Title</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repellendus voluptate quam aliquam dolore eos eaque sit placeat iste necessitatibus non quibusdam quia veniam, commodi at. Dolorem voluptatum inventore ducimus!
+            {data.title}
           </p>
           <div className={styles.author}>
             <Image
-              src=""
+              src={data.img}
               alt=""
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Span text</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src=""
+            src={data.img}
             alt=""
             fill={true}
             className={styles.image}
@@ -35,7 +50,7 @@ const BlogPost = () => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repellendus voluptate quam aliquam dolore eos eaque sit placeat iste necessitatibus non quibusdam quia veniam, commodi at. Dolorem voluptatum inventore ducimus!
+          {data.content}
         </p>
       </div>
     </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,11 +3,27 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
+async function getData(){
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts',{
+    cache: 'no-store'
+  });
+  
+  if(!res.ok){
+    throw new Error('Failed to fetch data');
+  }
+  
+  return res.json();
+}
 
-const Blog = () => {
+const Blog = async () => {
+  
+  const data = await getData();
+  
   return (
     <div className={styles.mainContainer}>
-      <Link href='/blog/testId' className={styles.container}>
+    
+    {data.map((item)=>(
+      <Link href='/blog/testId' className={styles.container} key={item.id}>
         <div className={styles.imageContainer}>
           <Image
             src=''
@@ -18,25 +34,11 @@ const Blog = () => {
           />
         </div>
         <div className={styles.content}>
-          <h1 className={styles.title}>Title testing</h1>
+          <h1 className={styles.title}>{item.title}</h1>
           <p className={styles.desc}>Desc</p>
         </div>
       </Link>
-      <Link href='/blog/testId' className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src=''
-            alt=''
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title testing</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
+    ))}
     </div>
   )
 }
