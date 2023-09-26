c7d4d42f7981966fcb855de4a902f84a70d2115b
ar.sam.cossa@gmail.com <ar.sam.cossa@gmail.com>
2023-09-26 03:19:54 +0200

    Estilizacao do botao drkmode

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image';
+
 
 const BlogPost = () => {
+  // const data = await getData(params.id);
   return (
-    <div>BlogPost</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repellendus voluptate quam aliquam dolore eos eaque sit placeat iste necessitatibus non quibusdam quia veniam, commodi at. Dolorem voluptatum inventore ducimus!
+          </p>
+          <div className={styles.author}>
+            <Image
+              src=""
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Span text</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src=""
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repellendus voluptate quam aliquam dolore eos eaque sit placeat iste necessitatibus non quibusdam quia veniam, commodi at. Dolorem voluptatum inventore ducimus!
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default BlogPost
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import styles from "./page.module.css";
+import Button from '@/components/Button/Button';
+import Image from 'next/image';
 
-const Category = () => {
+const Category = ({params}) => {
+  console.log(params)
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p  className={styles.desc}>Desc</p>
+          <Button text='See More' url='#'/>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image 
+            className={styles.desc}
+            fill={true} 
+            src='/'
+            alt=''
+            />
+        </div>
+      </div>
+      
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text='See More' url='#' />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.desc}
+            fill={true}
+            src='/'
+            alt=''
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
