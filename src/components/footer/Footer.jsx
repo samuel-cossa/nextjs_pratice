b53d74268dfd926d04448a371902f649cfefc63f
ar.sam.cossa@gmail.com <ar.sam.cossa@gmail.com>
2023-09-26 08:47:43 +0200

    Fist comit from create-next-app configuring my layout

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div>©2023 Lamamia. All rights reserved.</div>
+      
+    </div>
+  )
+}
+
+export default Footer
